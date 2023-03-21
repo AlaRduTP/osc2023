58880af85b86ca49ad550089dbc57045ab20c6d4
AlaRduTP <alardutp37259418@gmail.com>
2023-03-21 22:19:09 +0800

    Use QEMU `--pidfile` option.
    Use `--pidfile` option the get the pid of QEMU daemon. Also use
    `sed` and `grep`, instead of `awk`, to get the tty QEMU created.

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+read QEMU_PIDFILE < <(xxd -p -l 4 /dev/urandom)
 read QEMU_PTY \
-    < <(qemu-system-aarch64 -M raspi3b -kernel $1 -daemonize -display none -serial null -serial pty -initrd $3 -dtb $4 \
-    | awk '{print $5}')
+    < <(qemu-system-aarch64 -M raspi3b -kernel $1 -daemonize --pidfile $QEMU_PIDFILE -display none -serial null -serial pty -initrd $3 -dtb $4 \
+    | sed 's/ /\n/g' | grep '/dev')
 echo "QEMU serial port is attached to $QEMU_PTY"
 
 tmux new \; \
@@ -14,10 +15,8 @@ tmux new \; \
      selectp -t 1 \; \
      send 'bash' C-m "clear; while true; do read -p 'Press enter to load kernel ...'; ./$2 $QEMU_PTY; done" C-m
 
-read QEMU_DEAMON \
-    < <(ps -eo 'tty ppid pid command' \
-    | grep -i '^?.*qemu' \
-    | awk '{print $3}')
+read QEMU_DAEMON < <(cat $QEMU_PIDFILE)
+rm -f $QEMU_PIDFILE
 
 kill -9 $QEMU_DAEMON
 echo "QEMU daemon is killed (pid: $QEMU_DAEMON)"
